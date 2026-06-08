dd48dd745a65e10a40da0f8731788ba12f7084a6
MrWalium <coolcoderai123@gmail.com>
2026-06-07 19:01:39 -0700

    fixed formating

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -1,155 +1,125 @@
 package org.firstinspires.ftc.teamcode;
 
-
 // I don't think we will ever need to change this
-
-
 public class PIDController {
 
+    private double kp;
+    private double ki;
+    private double kd;
 
-   private double kp;
-   private double ki;
-   private double kd;
+    private double pError;
+    private double dError;
+    private double iError;
 
+    private double previousError = 0;
+    private double elapsedTime = 0;
 
-   private double pError;
-   private double dError;
-   private double iError;
+    private double deadZone = 0;
 
+    // depends if you want to make a "atGoal" method
+    // private double pTolerance = 0.05;
+    // private double dTolerance = Double.POSITIVE_INFINITY;
+    // because motors only output powers for -1 to 1
+    private double integralCeil = 0.5;
 
-   private double previousError = 0;
-   private double elapsedTime = 0;
+    private double lastTime = 0;
+    private boolean usingExternalClock;
 
+    public PIDController(double kp, double ki, double kd) {
+        this(kp, ki, kd, false);
+    }
 
-   private double deadZone = 0;
+    public PIDController(double kp, double ki, double kd, boolean usingExternalClock) {
+        this.kd = kd;
+        this.kp = kp;
+        this.ki = ki;
+        this.usingExternalClock = usingExternalClock;
+    }
 
+    public void setDeadZone(double d) {
+        deadZone = d;
+    }
 
-   // depends if you want to make a "atGoal" method
-//    private double pTolerance = 0.05;
-//    private double dTolerance = Double.POSITIVE_INFINITY;
+    public double calculate(double currPos, double goal) {
+        if (usingExternalClock) {
+            throw new SecurityException("Using an externalClock!!");
+        }
+        if (Math.abs(currPos - goal) <= deadZone) {
+            return 0;
+        }
 
+        double currentTime = (double) System.nanoTime() / 1E9;
 
-   // because motors only output powers for -1 to 1
-   private double integralCeil = 0.5;
+        if (lastTime == 0) lastTime = currentTime;
 
+        elapsedTime = currentTime - lastTime;
+        lastTime = currentTime;
 
-   private double lastTime = 0;
+        pError = goal - currPos;
 
+        //Filter out really small times
+        if (Math.abs(elapsedTime) > 1E-6) {
+            dError = (pError - previousError) / elapsedTime;
+        } else {
+            dError = 0;
+        }
 
-   private boolean usingExternalClock;
+        iError += elapsedTime * (pError);
+        iError = Math.abs(iError) > integralCeil ? integralCeil : iError;
 
+        previousError = pError;
 
-   public PIDController(double kp, double ki, double kd){
-       this(kp, ki, kd, false);
-   }
+        return kp * pError + kd * dError + ki * iError;
+    }
 
+    /**
+     * @param currPos     encoderTicks
+     * @param goal        encoder ticks
+     * @param currentTime Time should be in seconds
+     * @return output powers
+     */
+    public double calculate(double currPos, double goal, double currentTime) {
+        if (!usingExternalClock) {
+            usingExternalClock = true;
+        } else {
+            throw new RuntimeException("not using an externalClock!");
+        }
 
-   public PIDController(double kp, double ki, double kd, boolean usingExternalClock){
-       this.kd = kd;
-       this.kp = kp;
-       this.ki = ki;
-       this.usingExternalClock = usingExternalClock;
-   }
+        if (lastTime == 0) lastTime = currentTime;
 
+        elapsedTime = currentTime - lastTime;
+        lastTime = currentTime;
 
-   public void setDeadZone(double d){
-       deadZone = d;
-   }
+        pError = goal - currPos;
 
+        //Filter out really small times
+        if (Math.abs(elapsedTime) > 1E-6) {
+            dError = (pError - previousError) / elapsedTime;
+        } else {
+            dError = 0;
+        }
 
-   public double calculate(double currPos, double goal){
-       if(usingExternalClock){throw new SecurityException("Using an externalClock!!");}
-       if (Math.abs(currPos - goal) <= deadZone){return 0;}
+        iError += elapsedTime * (pError);
+        iError = Math.abs(iError) > integralCeil ? integralCeil * Math.signum(iError) : iError;
 
+        return kp * pError + kd * dError + ki * iError;
+    }
 
-       double currentTime = (double) System.nanoTime() / 1E9;
+    public void reset() {
+        lastTime = 0;
+    }
 
+    // Method to tune the PID gains
+    public void setPID(double kP, double ki, double kd) {
+        this.kp = kP;
+        this.ki = ki;
+        this.kd = kd;
+    }
 
-       if (lastTime == 0) lastTime = currentTime;
+    public void resetIntegral() {
+        iError = 0;
+    }
 
-
-       elapsedTime = currentTime - lastTime;
-       lastTime = currentTime;
-
-
-       pError = goal - currPos;
-
-
-       //Filter out really small times
-       if (Math.abs(elapsedTime) > 1E-6) {
-           dError = (pError - previousError) / elapsedTime;
-       } else {
-           dError = 0;
-       }
-
-
-       iError += elapsedTime * (pError);
-       iError = Math.abs(iError) > integralCeil ? integralCeil : iError;
-
-
-       previousError = pError;
-
-
-       return kp * pError + kd * dError + ki * iError;
-   }
-
-
-   /**
-    * @param currPos encoderTicks
-    * @param goal encoder ticks
-    * @param currentTime Time should be in seconds
-    * @return output powers
-    */
-   public double calculate(double currPos, double goal, double currentTime){
-       if (!usingExternalClock) {usingExternalClock = true;}
-       else { throw new RuntimeException("not using an externalClock!");}
-
-
-       if (lastTime == 0) lastTime = currentTime;
-
-
-       elapsedTime = currentTime - lastTime;
-       lastTime = currentTime;
-
-
-       pError = goal - currPos;
-
-
-       //Filter out really small times
-       if (Math.abs(elapsedTime) > 1E-6) {
-           dError = (pError - previousError) / elapsedTime;
-       } else {
-           dError = 0;
-       }
-
-
-       iError += elapsedTime * (pError);
-       iError = Math.abs(iError) > integralCeil ? integralCeil * Math.signum(iError) : iError;
-
-
-       return kp * pError + kd * dError + ki * iError;
-   }
-
-
-   public void reset(){
-       lastTime = 0;
-   }
-
-
-   // Method to tune the PID gains
-   public void setPID(double kP, double ki, double kd) {
-       this.kp = kP;
-       this.ki = ki;
-       this.kd = kd;
-   }
-
-
-   public void resetIntegral(){
-       iError = 0;
-   }
-
-
-   public void setIntegralCeil() {}
-
-
+    public void setIntegralCeil() {
+    }
 }
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretPIDTuner.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretPIDTuner.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import androidx.collection.ArraySet;
-
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -11,165 +9,103 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
 import com.acmerobotics.dashboard.config.Config;
-
-
-
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
 import java.util.ArrayList;
 
-
 @Config
-@TeleOp(name="TurretPIDTuner")
+@TeleOp(name = "TurretPIDTuner")
 public class TurretPIDTuner extends LinearOpMode {
 
+    public static double kp = 0.00025, ki = 0.0075, kd = 0.00000155;
+    public static int wantedrpm = 0;
+    public static double kf = 0.0002;
+    public static double queueSize = 5;
+    public double lKp = kp, lKi = ki, lKd = kd;
+    public DcMotor motor;
+    public double cooldown = -100;
+    public boolean debounce = true;
+    double time = 0;
+    Telemetry tele;
+    double prevPos = 0;
+    double currPos = 0;
+    private PIDController rpmcontroller;
+    private ArrayList<Double> records = new ArrayList<>();
 
-   public static double kp = 0.00025, ki = 0.0075, kd = 0.00000155;
-   public double lKp = kp, lKi = ki, lKd = kd;
+    @Override
+    public void runOpMode() {
 
+        tele = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-   public DcMotor motor;
+        rpmcontroller = new PIDController(kp, ki, kd);
 
+        motor = hardwareMap.dcMotor.get("turret");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        // motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        waitForStart();
+        if (isStopRequested()) return;
 
+        while (opModeIsActive()) {
+            tele.addData("WantedRPM", wantedrpm);
+            tele.update();
 
-   double time = 0;
+            if (kp != lKp || ki != lKi || kd != lKd) {
+                rpmcontroller.setPID(kp, ki, kd);
 
+                lKp = kp;
+                lKi = ki;
+                lKd = kd;
+            }
 
-   public static int wantedrpm = 0;
+            pidtunedmotor(wantedrpm);
+        }
+    }
 
+    public void pidtunedmotor(double rpm) {
+        prevPos = currPos;
+        currPos = motor.getCurrentPosition();
 
-   private PIDController rpmcontroller;
-   private ArrayList<Double> records = new ArrayList<>();
-   Telemetry tele;
-   @Override
-   public void runOpMode() {
+        tele.addData("pos", motor.getCurrentPosition());
+        double dTheta = (currPos - prevPos) / 28;   //rotations?
 
+        double dt = System.nanoTime() / 1e9 - time;
+        time = System.nanoTime() / 1e9;
 
-       tele = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        double currRPM = dTheta / (dt / 60);
 
+        //stop calculations if rpm cant be actually calculated
+        if (currRPM <= 0 && cooldown < 0.25) {
+            cooldown += dt;
+            return;
+        }
+        if (currRPM > 0) cooldown = 0;
 
-       rpmcontroller = new PIDController(kp,ki,kd);
+        records.add(currRPM);
+        while (records.size() > queueSize) records.remove(0);
 
+        double undividedAverage = 0;
 
-       motor = hardwareMap.dcMotor.get("turret");
-       motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-      // motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      // motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        for (int i = 0; i < records.size(); i++) {
+            undividedAverage += records.get(i);
+        }
 
+        double average;
 
+        if (records.size() == queueSize) average = undividedAverage / queueSize;
+        else average = currRPM;
 
+        tele.addData("average", average);
+        tele.addData("CurrRPM", currRPM);
+        double wantedWheelPowerAverage = rpmcontroller.calculate(average, rpm) + (wantedrpm * kf);
+        if (wantedWheelPowerAverage == 0) wantedWheelPowerAverage = wantedrpm;
 
-       waitForStart();
-       if (isStopRequested()) return;
+        tele.addData("AttemptedRPM", wantedWheelPowerAverage);
 
-
-
-
-       while (opModeIsActive()) {
-           tele.addData("WantedRPM", wantedrpm);
-           tele.update();
-
-
-
-
-
-
-           if (kp != lKp || ki != lKi || kd != lKd) {
-               rpmcontroller.setPID(kp, ki, kd);
-
-
-               lKp = kp;
-               lKi = ki;
-               lKd = kd;
-           }
-
-
-           pidtunedmotor(wantedrpm);
-       }
-   }
-
-
-   double prevPos = 0;
-   double currPos = 0;
-
-
-   public static double kf = 0.0002;
-
-
-   public static double queueSize = 5;
-
-
-   public double cooldown = -100;
-   public boolean debounce = true;
-   public void pidtunedmotor(double rpm) {
-       prevPos = currPos;
-       currPos = motor.getCurrentPosition();
-
-
-
-
-       tele.addData("pos", motor.getCurrentPosition());
-       double dTheta = (currPos - prevPos) / 28;   //rotations?
-
-
-       double dt = System.nanoTime() / 1e9 - time;
-       time = System.nanoTime() / 1e9;
-
-
-
-
-       double currRPM = dTheta / (dt / 60);
-
-
-       //stop calculations if rpm cant be actually calculated
-       if (currRPM <= 0 && cooldown < 0.25) {
-           cooldown += dt;
-           return;
-       }
-       if (currRPM > 0) cooldown = 0;
-
-
-
-
-       records.add(currRPM);
-       while (records.size() > queueSize)
-           records.remove(0);
-
-
-
-
-       double undividedAverage = 0;
-
-
-       for (int i = 0; i < records.size(); i++)
-           undividedAverage += records.get(i);
-
-
-
-
-       double average;
-
-
-       if (records.size() == queueSize) average = undividedAverage / queueSize;
-       else average = currRPM;
-
-
-       tele.addData("average", average);
-       tele.addData("CurrRPM", currRPM);
-       double wantedWheelPowerAverage = rpmcontroller.calculate(average, rpm) + (wantedrpm * kf);
-       if (wantedWheelPowerAverage == 0) wantedWheelPowerAverage = wantedrpm;
-
-
-       tele.addData("AttemptedRPM", wantedWheelPowerAverage);
-
-
-       motor.setPower(wantedrpm == 0 ? 0 : wantedWheelPowerAverage);
-
-
-   }
+        motor.setPower(wantedrpm == 0 ? 0 : wantedWheelPowerAverage);
+    }
 }
